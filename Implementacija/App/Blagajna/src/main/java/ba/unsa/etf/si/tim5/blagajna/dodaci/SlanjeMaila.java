c4cb7358ec07fba3bd686b72642054363b37ffc2
Faris <fdzafic1@etf.unsa.ba>
2015-05-21 17:14:18 +0200

    haman nista

--- Implementacija/App/Blagajna/src/main/java/ba/unsa/etf/si/tim5/blagajna/dodaci/SlanjeMaila.java
+++ Implementacija/App/Blagajna/src/main/java/ba/unsa/etf/si/tim5/blagajna/dodaci/SlanjeMaila.java
@@ -2,10 +2,20 @@ package ba.unsa.etf.si.tim5.blagajna.dodaci;
 
 import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class SlanjeMaila {
 
 	private static SlanjeMaila instanca = null;
-
+	private String from = "faris.dzafic";
+	private String pass = "loveisgone";
+	
 	public static SlanjeMaila getInstance() {
 		if (instanca == null)
 			instanca = new SlanjeMaila();
@@ -15,17 +25,18 @@ public class SlanjeMaila {
 	public SlanjeMaila() {
 
 	}
-
-	public void sendFromGMail(String from, String pass, String[] to,
+	
+	public void sendFromGMail(String[] to,
 			String subject, String body) {
 		Properties props = System.getProperties();
 		String host = "smtp.gmail.com";
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.starttls.enable", "true");		
 		props.put("mail.smtp.user", from);
 		props.put("mail.smtp.password", pass);
-		props.put("mail.smtp.port", "587");
 		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
 
 		Session session = Session.getDefaultInstance(props);
 		MimeMessage message = new MimeMessage(session);
--- Implementacija/App/Blagajna/src/main/java/ba/unsa/etf/si/tim5/blagajna/dodaci/UpozorenjeZaDug.java
+++ Implementacija/App/Blagajna/src/main/java/ba/unsa/etf/si/tim5/blagajna/dodaci/UpozorenjeZaDug.java
@@ -4,21 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import org.hibernate.Session;
-
+import ba.unsa.etf.si.tim5.blagajna.dodaci.PorukaMail;
 import ba.unsa.etf.si.tim5.blagajna.entiteti.Dug;
 import ba.unsa.etf.si.tim5.blagajna.entiteti.Rata;
-import ba.unsa.etf.si.tim5.blagajna.entiteti.Student;
 import ba.unsa.etf.si.tim5.blagajna.util.HibernateUtil;
 
-public class UpozorenjeZaDug {
+public class UpozorenjeZaDug {	
 	
 	private ArrayList<Dug> dugovi;
-	private ArrayList<String> mailoviStudenata;
-	private ArrayList<String> poruke;
+	private ArrayList<PorukaMail> poruke;
 	
 	public UpozorenjeZaDug() {
-		mailoviStudenata = new ArrayList<String>();
-		poruke = new ArrayList<String>();
+		
 	}
 	
 	 
@@ -28,17 +25,21 @@ public class UpozorenjeZaDug {
 	}
 	
 	public void provjeriDugove() {
+		poruke = new ArrayList<PorukaMail>();
 		dugovi = Dao.getInstance().dajSveDugove("SELECT * FROM DUG WHERE JELIIZMIREN = 0;");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		for(int i=0; i<dugovi.size(); i++) {
 			ArrayList<Rata> rate = dugovi.get(i).dajSveRate(session);			
 			
 			for(int j = 0; j < rate.size(); j++) {
-				if(Math.abs(dajRazlikuDatuma(rate.get(j).getRokUplate(), new Date())) < 30) {
-					mailoviStudenata.add(dugovi.get(i).dajStudenta().getMail());
+				if(Math.abs(dajRazlikuDatuma(rate.get(j).getRokUplate(), new Date())) < 30) {					
+					PorukaMail pm = new PorukaMail();
+					
 					if(dugovi.get(i).getTipDuga() == TipDuga.dugZaLiteraturu) 
-						poruke.add("Niste izmirili svoje obaveze prema fakultetu. Imate neisplaćene dugove za literaturu !");
-					else poruke.add("Niste izmirili svoje obaveze prema fakultetu. Imate neisplaćene dugove za školarinu !");
+						pm.postaviDugZaLiteraturu(dugovi.get(i).dajStudenta().getMail(), rate.get(i).getRokUplate().toString());
+					else pm.postaviDugZaSkolarinu(dugovi.get(i).dajStudenta().getMail(), rate.get(i).getRokUplate().toString());
+					
+					poruke.add(pm);
 					break;
 				}
 			}				
@@ -50,6 +51,14 @@ public class UpozorenjeZaDug {
 	
 	
 	public void posaljiMail() {
+		if(poruke == null) provjeriDugove();
+		if(poruke.size() == 0) return;
+	
+		for(int i = 0; i<poruke.size(); i++) {
+			String[] mail = {poruke.get(i).getMail()};
+			SlanjeMaila.getInstance().sendFromGMail(mail, poruke.get(i).getNaslov(), poruke.get(i).getPoruka());
+			
+		}
 		
 	}
 }
